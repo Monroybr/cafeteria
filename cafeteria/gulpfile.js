189d94948f5d473eb6dbf8fec21bccb6affef4a3
liseth <liseth052009@gmail.com>
2026-01-19 11:37:34 -0500

    Creacion de navehacion principal en index.html

--- cafeteria/gulpfile.js
+++ cafeteria/gulpfile.js
@@ -1,8 +1,27 @@
+const {src, dest, watch, series, parallel} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+//NOTA: cuando aparecen las llaves significa que exporta varios y cuando no tiene solo exporta uno
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-function tarea(done){
-    console.log('Desde mi primer tarea');
+function css( done){
+    //compilar sass
+    //pasos: -1 identificar archivo, 2 compilarla, 3-guardar el .css
+    src('src/scss/app.scss') //identificar el archivo
+        .pipe( sass() ) //compilarlo -- a la funcion sass se le puede pasar un objeto JS
+        .pipe( postcss( [autoprefixer() ] ))
+        .pipe(dest('build/css')) //guardar
 
     done();
 }
 
-exports.tarea = tarea;
+function dev(){
+    watch('src/scc/**/*.scss', css);
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.default = series(css, dev);
+
+//series- se inicia una tarea y hasta que finaliza inicia la siguiente
+//parallel - todas inician al mismo tiempo
